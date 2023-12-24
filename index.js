0b0c30c550e16e5b2137b3a2ed6d8913410e40d6
adhfarg <153331802+adhfarg@users.noreply.github.com>
2023-12-24 16:29:17 -0500

    ”completed”

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const newHeader = document.createElement("h1"); 
+
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion";
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+
